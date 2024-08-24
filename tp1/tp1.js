088947f9fd13fe861eb1ad55ed42ad3131a0cfcb
maitepereyra <167358224+maitepereyra@users.noreply.github.com>
2024-08-24 20:00:57 -0300

    Add files via upload

---
+++ tp1/tp1.js
@@ -0,0 +1,31 @@
+let imagen
+let colores = true;
+
+function preload() {
+  imagen = loadImage('data/obra.png');
+  }
+
+function setup() {
+ createCanvas(800,400);
+ rectMode(CENTER);
+ noLoop();
+}
+
+
+function draw() {
+background(255);
+image(imagen,0,0);
+estructura (28, width/2, 0);
+}
+
+function mousePressed () {
+  colores = true;
+  redraw();
+}
+
+function keyPressed () {
+  if (key == 'g') {
+    colores = false;
+    redraw();
+  }
+}
