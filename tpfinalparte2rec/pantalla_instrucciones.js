0e1b0fde847068a06830c100b7aff1afba2b86f2
maitepereyra <167358224+maitepereyra@users.noreply.github.com>
2024-12-08 02:07:01 -0300

    Update pantalla_instrucciones.js

--- tpfinalparte2rec/pantalla_instrucciones.js
+++ tpfinalparte2rec/pantalla_instrucciones.js
@@ -1,23 +1,15 @@
-class pantallaInicio {
+class pantallaInstrucciones {
   constructor() {
   }
 
   dibujar() {
-    image(imgInicio, 0, 0, width, height);
+    image(imgInstrucciones, 0, 0, width, height);
   }
 
-  botonJugar() {
+  boton() {
     if (mousePressed) {
-      if (mouseX > 225 && mouseX < 410 && mouseY > 125 && mouseY < 185) {
+      if (mouseX > 265 && mouseX < 400 && mouseY > 300 && mouseY < 335) {
         objPrincipal.controlDePantalla.pantallactiva = 3;
-      }
-    }
-  }
-
-  botonInstrucciones() {
-    if (mousePressed) {
-      if (mouseX > 250 && mouseX < 405 && mouseY > 200 && mouseY < 255) {
-        objPrincipal.controlDePantalla.pantallactiva = 2;
       }
     }
   }
