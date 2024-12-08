a4f63f7915edb074eb5edaeaeff664387c65cb85
maitepereyra <167358224+maitepereyra@users.noreply.github.com>
2024-12-08 01:27:20 -0300

    Add files via upload

---
+++ tpfinalparte1/tpfinalparte1.js
@@ -0,0 +1,40 @@
+//TPFINALPARTE1
+//MAITENA PEREYRA PROPATTO 120354/3
+//COMISION 5
+//LINK DEL VIDEO:
+
+let anchoPantalla = 640;
+let altoPantalla = 480;
+let imagenes = [];
+let texto = [];
+let cantidadImg = 23;
+let pantallaActual = 0;
+let lineaTexto = 0;
+let boton, botonCreditos, botonReiniciar;
+let imagenIntro, imagenCreditos;
+let decision = false;
+let fuente;
+
+function setup() {
+  createCanvas(anchoPantalla, altoPantalla);
+}
+
+
+function draw() {
+  if (pantallaActual === 0) {
+    image(imagenIntro, 0, 0, anchoPantalla, altoPantalla);
+  } else if (pantallaActual === "creditos") {
+    creditos();
+    dibujarBotonReiniciar();
+    if (!sound.isPlaying()) {
+      sound.play();
+    }
+  } else {
+    dibujarPantalla(pantallaActual);
+  }
+}
+
+function mousePressed() {
+  botones();
+  console.log(mouseX, mouseY);
+}
