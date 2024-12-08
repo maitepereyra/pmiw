078310a37d57b67ab432b45cf2ad423918e3bdb4
maitepereyra <167358224+maitepereyra@users.noreply.github.com>
2024-12-08 03:31:20 -0300

    Update tpfinalparte2rec.js

--- tpfinalparte2rec/tpfinalparte2rec.js
+++ tpfinalparte2rec/tpfinalparte2rec.js
@@ -1,6 +1,7 @@
 //TPFINALPARTE2
 //MAITENA PEREYRA PROPATTO 120354/3
 //COMISION 5
+// hola profe, disculpe comunicarme por estos medios pero queria aclarar que todo el trabajo lo estuve haciendo bajo un nombre de prueba, al momento de subir el trabajo final me tome el tiempo de cambiar el nombre para cumplir los requisitos. al momento que lo hice, funcionaba a la perfeccion, sin embargo, ahora me tira un error que no permite ver nada, dejo mi codigo "de prueba" para que se comprenda que son exactamente iguales y no hay razones oara que uno funcione y el otro no
 
 let imgArma, imgBase, imgEnemigo, imgFondo, imgPersonaje, imgProyectil, imgInicio, imgInstrucciones, imgCreditos, imgPerdiste, imgGanaste;
 let objPrincipal;
