a742fb3654c620f9db97a06d9d89023dc4c7125b
maitepereyra <167358224+maitepereyra@users.noreply.github.com>
2024-12-10 16:38:44 -0300

    Add files via upload

---
+++ Etapa 3/recuprueba/pantalla_creditos.js
@@ -0,0 +1,16 @@
+class pantallaCreditos {
+  constructor() {
+  }
+
+  dibujar() {
+    image(imgCreditos, 0, 0, width, height);
+  }
+
+  botonReiniciar() {
+    if (mousePressed) {
+      if (mouseX > 240 && mouseX < 420 && mouseY > 300 && mouseY < 355) {
+        objPrincipal.reiniciarJuego();
+      }
+    }
+  }
+}
